b6546b8525931834c97e6ef3ba3ef871f5149fee
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-04 02:51:56 +0200

    X6739: Make libformatter MTK specific
    * Avoid collisions with Car libraries.
    
    Change-Id: I602b1d65fdf672244a91a3f7c4a71470beae7ea4
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -91,6 +91,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('STNFC_FW_DEBUG_ENABLED=1', 'STNFC_FW_DEBUG_ENABLED=0'),
     'vendor/lib64/librt_extamp_intf.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+     ('vendor/lib64/libcodec2_mtk_venc.so', 'vendor/lib64/libcodec2_mtk_vdec.so'): blob_fixup()
+        .replace_needed('libformatter.so', 'libformatter_mtk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
