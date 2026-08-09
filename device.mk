38242b626254b6b4847c412c46850de8bd198de2
vlxlxlv <fajarslebew31@gmail.com>
2026-08-09 17:35:02 +0200

    Revert "X6739: Dolby Atmos : Activate"
    This reverts commit 28dc20cfc63a6c24dcb86e904f4ddb1357bed545.
    
    Change-Id: I8e08a8682223070347cf12d0996aa28f3bd11256
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -135,9 +135,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     ANGLE
 
-# Dolby
-$(call inherit-product, vendor/sony/dolby/setup.mk)
-
 # DRM
 PRODUCT_PACKAGES += \
     com.android.hardware.drm.clearkey
